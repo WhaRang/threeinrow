ee887df25ddf7397ebf788e765a4b70c2cad8296
BloodySoySteak <aleksei.tokareff@gmail.com>
2025-01-27 21:03:01 +0100

    imported text mesh pro. added EntityField script

---
+++ Assets/Scripts/Framework/Entities/EntityField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Framework.Entities
+{
+    public class EntityField
+    {
+        private Entity[] entities;
+
+        public EntityField()
+        {
+            entities = Array.Empty<Entity>();
+        }
+
+        public EntityField(int size)
+        {
+            entities = new Entity[size];
+            for (int i = 0; i < entities.Length; i++)
+            {
+                entities[i] = new Entity();
+            }
+        }
+
+        public EntityField(Entity[] entities)
+        {
+            this.entities = entities;
+        }
+    }
+}
