4e97acfe572335e586c8ada7c6600653bbd1e0f5
BloodySoySteak <aleksei.tokareff@gmail.com>
2025-01-27 20:00:43 +0100

    added tests for entity equals operators

---
+++ Assets/Scripts/Tests/EntitiesTests/CharEntityTests.cs
@@ -0,0 +1,111 @@
+﻿using Framework.Entities;
+using NUnit.Framework;
+
+namespace Tests.EntitiesTests
+{
+    [TestFixture]
+    public class CharEntityTests
+    {
+        [TestCase]
+        public void Test_Equals()
+        {
+            // Arrange
+            CharEntity a1 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+
+            CharEntity a2 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+            
+            CharEntity b = new CharEntity
+            {
+                Symbol = 'B'
+            };
+
+            Entity e = new Entity();
+            
+            // Act
+            bool a1EqualsA2 = a1.Equals(a2);
+            bool a1EqualsB = a1.Equals(b);
+            bool a1EqualsE = a1.Equals(e);
+            bool a1EqualsNull = a1.Equals(null);
+            
+            // Assert
+            Assert.AreEqual(a1EqualsA2, true);
+            Assert.AreEqual(a1EqualsB, false);
+            Assert.AreEqual(a1EqualsE, false);
+            Assert.AreEqual(a1EqualsNull, false);
+        }
+
+        [TestCase]
+        public void Test_Equals_Operator()
+        {
+            // Arrange
+            CharEntity a1 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+
+            CharEntity a2 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+            
+            CharEntity b = new CharEntity
+            {
+                Symbol = 'B'
+            };
+
+            Entity e = new Entity();
+            
+            // Act
+            bool a1EqualsA2 = a1 == a2;
+            bool a1EqualsB = a1 == b;
+            bool a1EqualsE = a1 == e;
+            bool a1EqualsNull = a1 == null;
+            
+            // Assert
+            Assert.AreEqual(a1EqualsA2, true);
+            Assert.AreEqual(a1EqualsB, false);
+            Assert.AreEqual(a1EqualsE, false);
+            Assert.AreEqual(a1EqualsNull, false);
+        }
+
+        [TestCase]
+        public void Test_Not_Equals_Operator()
+        {
+            // Arrange
+            CharEntity a1 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+
+            CharEntity a2 = new CharEntity
+            {
+                Symbol = 'A'
+            };
+            
+            CharEntity b = new CharEntity
+            {
+                Symbol = 'B'
+            };
+
+            Entity e = new Entity();
+            
+            // Act
+            bool a1EqualsA2 = a1 != a2;
+            bool a1EqualsB = a1 != b;
+            bool a1EqualsE = a1 != e;
+            bool a1EqualsNull = a1 != null;
+            
+            // Assert
+            Assert.AreEqual(a1EqualsA2, false);
+            Assert.AreEqual(a1EqualsB, true);
+            Assert.AreEqual(a1EqualsE, true);
+            Assert.AreEqual(a1EqualsNull, true);
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/EntitiesTests/EntityTests.cs
@@ -0,0 +1,87 @@
+﻿using Framework.Entities;
+using NUnit.Framework;
+
+namespace Tests.EntitiesTests
+{
+    [TestFixture]
+    public class EntityTests
+    {
+        [TestCase]
+        public void Test_Equals()
+        {
+            // Arrange
+            Entity e1 = new Entity();
+            Entity e2 = new Entity();
+            
+            // Act
+            bool e1EqualsE2 = e1.Equals(e2);
+            bool e2EqualsE1 = e2.Equals(e1);
+            bool e1EqualsNull = e1.Equals(null);
+            bool e2EqualsNull = e2.Equals(null);
+            
+            bool e1EqualsE1 = e1.Equals(e1);
+            bool e2EqualsE2 = e2.Equals(e2);
+            
+            // Assert
+            Assert.AreEqual(e1EqualsE2, false);
+            Assert.AreEqual(e2EqualsE1, false);
+            Assert.AreEqual(e1EqualsNull, false);
+            Assert.AreEqual(e2EqualsNull, false);
+            
+            Assert.AreEqual(e1EqualsE1, true);
+            Assert.AreEqual(e2EqualsE2, true);
+        }
+
+        [TestCase]
+        public void Test_Equals_Operator()
+        {
+            // Arrange
+            Entity e1 = new Entity();
+            Entity e2 = new Entity();
+            
+            // Act
+            bool e1EqualsE2 = e1 == e2;
+            bool e2EqualsE1 = e2 == e1;
+            bool e1EqualsNull = e1 == null;
+            bool e2EqualsNull = e2 == null;
+            
+            bool e1EqualsE1 = e1 == e1;
+            bool e2EqualsE2 = e2 == e2;
+            
+            // Assert
+            Assert.AreEqual(e1EqualsE2, false);
+            Assert.AreEqual(e2EqualsE1, false);
+            Assert.AreEqual(e1EqualsNull, false);
+            Assert.AreEqual(e2EqualsNull, false);
+            
+            Assert.AreEqual(e1EqualsE1, true);
+            Assert.AreEqual(e2EqualsE2, true);
+        }
+        
+        [TestCase]
+        public void Test_Not_Equals_Operator()
+        {
+            // Arrange
+            Entity e1 = new Entity();
+            Entity e2 = new Entity();
+            
+            // Act
+            bool e1EqualsE2 = e1 != e2;
+            bool e2EqualsE1 = e2 != e1;
+            bool e1EqualsNull = e1 != null;
+            bool e2EqualsNull = e2 != null;
+            
+            bool e1EqualsE1 = e1 != e1;
+            bool e2EqualsE2 = e2 != e2;
+            
+            // Assert
+            Assert.AreEqual(e1EqualsE2, true);
+            Assert.AreEqual(e2EqualsE1, true);
+            Assert.AreEqual(e1EqualsNull, true);
+            Assert.AreEqual(e2EqualsNull, true);
+            
+            Assert.AreEqual(e1EqualsE1, false);
+            Assert.AreEqual(e2EqualsE2, false);
+        }
+    }
+}
